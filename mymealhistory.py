7a150c1eaf91692454db81bb3f15afc0e5266c17
nissan2ford <48442727+nissan2ford@users.noreply.github.com>
2019-03-15 15:50:18 +0900

    Update mymealhistory.py
    20190315 JSON DATA

--- mymealhistory.py
+++ mymealhistory.py
@@ -1,11 +1,46 @@
 import datetime
+import json
+import sys
 
-today = datetime.datetime.now()
+print('Input MealTime.\nyyyymmdd or Hit Enter to set today')
+inputdata = input('>>> ')
 
-todaystr = '{0:%Y%m%d}'.format(today)
+if len(inputdata.rstrip()) == 0:
+  mealdate = int('{0:%Y%m%d}'.format(datetime.datetime.now()))
+elif len(inputdata.rstrip()) == 8:
+  mealdate = int(inputdata)
+else:
+  print('Invalid MealDate!')
+  sys.exit()
 
-print('todaystr = '+todaystr)
+print('mealdate = ',mealdate)
 
-todaywday = today.strftime('%a')
+mealdatestr = str(mealdate)
+mealdatedt = datetime.datetime.strptime(mealdatestr,'%Y%m%d')
 
-print('todaywday = '+todaywday)
+print('mealdatestr = '+mealdatestr)
+
+mealdatewday = mealdatedt.strftime('%a')
+
+print('todaywday = '+mealdatewday)
+
+mealtime = 'MOR'
+
+print('Input MealMethod.\nCandidate:"Hom","Out","Onl","Nop","Buy"')
+mealmethod = input('>>> ')
+mealmethodcand = ['Hom','Out','Onl','Nop','Buy']
+
+if not any((s == mealmethod) for s in mealmethodcand):
+  print('Invalid MealMethod!')
+  sys.exit()
+
+dic = {}
+
+
+dic["MealTime"] = mealtime
+dic["Date"] = mealdate
+dic["Day_of_week"] = mealdatewday
+dic["MealMethod"] = mealmethod
+
+json_dict = json.dumps(dic)
+print(json_dict)
